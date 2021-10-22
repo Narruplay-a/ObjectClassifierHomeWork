6bb174751b296b59481ec36c39e02efc2f6cf062
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2021-10-22 09:58:56 +0300

    Initial Commit

---
+++ HomeWorkCoreML/HomeWorkCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkCoreMLApp.swift
+//  HomeWorkCoreML
+//
+//  Created by Adel Khaziakhmetov on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
